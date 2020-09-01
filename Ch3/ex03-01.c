d32002565f71bd09a30f9d75f0b088724c95d03b
J Park <47515765+selenak666@users.noreply.github.com>
2020-09-01 18:34:21 +0900

    Create ex03-01.c

---
+++ Ch3/ex03-01.c
@@ -0,0 +1,36 @@
+/* program ex03-01.c */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+
+        int filedes, fdnew1, fdnew2;
+        ssize_t nread;
+        off_t newpos;
+
+        char buffer[1024];
+        char content[] = "Hello my friend!!\n";
+
+        filedes = open("data.txt", O_RDWR);
+
+        nread = read(filedes, buffer, 1024);
+        printf("%s", buffer);
+
+        write(filedes, content, strlen(content));
+
+        newpos = lseek(filedes, (off_t)0, SEEK_SET);
+        nread = read(filedes, buffer, 1024);
+        printf("%s", buffer);
+
+        close(filedes);
+
+        fdnes1 = open("newdata1.txt", O_RDWR | O_CREAT, 0644);
+        fdnew2 = create("newdata2.txt", 0644);
+
+        close(fdnew1); close(fdnew2);
+        unlink("newdata2.txt");
+
+}
